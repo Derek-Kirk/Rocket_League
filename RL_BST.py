7baeec2324b21582ebd87993f277320788ae5b4d
David Young <david44young@gmail.com>
2023-04-07 19:35:35 -0500

    attempting to fix RL_BST module

--- RL_BST.py
+++ RL_BST.py
@@ -1,37 +1,45 @@
 class BST:
 
-    import Node
+    def _init_(self):
+        from RL_Node import Node
+        self.root = Node()
 
-    def _init_(self, root):
-        self.root = None
+    def __str__(self):
+        return f'This is the root {str(self.root)}'
 
-    def comp(self, root, node):
-        if node.rating > root.rating:
-            node.right_child = root
-            node.left_child = root.left_child
-            root.left_child = root.right_child.left_child
+    def add_node(self, node):
+        if self.root is None:
+            self.root = node
+            print(f'New root: {str(node)}')
+            return
+
+        if node.rating > self.root.rating:
+            node.right_child = self.root
+            node.left_child = self.root.left_child
+            self.root.left_child = self.root.right_child.left_child
             self.root = node
 
-        elif node.rating < root.rating:
-            node.left_child = root.left_child.left_child
-            node.right_child = root.left_child.right_child
-            root.left_child = node
+        elif node.rating < self.root.rating:
+            node.left_child = self.root.left_child.left_child
+            node.right_child = self.root.left_child.right_child
+            self.root.left_child = node
 
         else:
-            if node.score > root.score:
-                node.right_child = root
-                node.left_child = root.left_child
-                root.left_child = root.right_child.left_child
+            if node.score > self.root.score:
+                node.right_child = self.root
+                node.left_child = self.root.left_child
+                self.root.left_child = self.root.right_child.left_child
                 self.root = node
 
-            elif node.score < root.score:
-                node.left_child = root.left_child.left_child
-                node.right_child = root.left_child.right_child
-                root.left_child = node
+            elif node.score < self.root.score:
+                node.left_child = self.root.left_child.left_child
+                node.right_child = self.root.left_child.right_child
+                self.root.left_child = node
+        return "node has been added"
 
     def search(self, lower, upper, node=None):
         print("This range is inclusive")
-        if self.root == None:
+        if self.root is None:
             print("There's no root")
             return
 
@@ -47,12 +55,3 @@ class BST:
 
             if node.right_child != None:
                 return(self.search(lower, upper, node.right_child))
-
-    def add_node(self, node):
-        if self.root == None:
-            self.root = node
-            print(f'the root is {node}')
-            return
-
-        self.comp(node)
-        return
