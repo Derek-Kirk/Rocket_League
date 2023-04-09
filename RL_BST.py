395176592870af3f1a6b68165a04bf746ab657d9
David Young <david44young@gmail.com>
2023-04-09 14:52:00 -0500

    Fixed __init__ for BST class and add/search func

--- RL_BST.py
+++ RL_BST.py
@@ -1,57 +1,52 @@
 class BST:
 
-    def _init_(self):
+    def __init__(self):
         from RL_Node import Node
-        self.root = Node()
+        self.root = None
 
     def __str__(self):
         return f'This is the root {str(self.root)}'
 
-    def add_node(self, node):
+    def add_node(self, node, next_node=None):
+
         if self.root is None:
             self.root = node
-            print(f'New root: {str(node)}')
-            return
+            return f'New root: {str(node)}'
 
-        if node.rating > self.root.rating:
-            node.right_child = self.root
-            node.left_child = self.root.left_child
-            self.root.left_child = self.root.right_child.left_child
-            self.root = node
+        if next_node is None:
+            next_node = self.root
 
-        elif node.rating < self.root.rating:
-            node.left_child = self.root.left_child.left_child
-            node.right_child = self.root.left_child.right_child
-            self.root.left_child = node
+        elif node.rating > next_node.rating:
+            if next_node.right_child is None:
+                next_node.right_child = node
 
-        else:
-            if node.score > self.root.score:
-                node.right_child = self.root
-                node.left_child = self.root.left_child
-                self.root.left_child = self.root.right_child.left_child
-                self.root = node
+            return self.add_node(node, next_node.right_child)
 
-            elif node.score < self.root.score:
-                node.left_child = self.root.left_child.left_child
-                node.right_child = self.root.left_child.right_child
-                self.root.left_child = node
+        elif node.rating < next_node.rating:
+            if next_node.left_child is None:
+                next.node.left_child = node
+
+            return self.add_node(node, next_node.left_child)
+
         return "node has been added"
 
     def search(self, lower, upper, node=None):
+        matches = []
         print("This range is inclusive")
         if self.root is None:
-            print("There's no root")
-            return
+            return("There's no root")
 
-        if node == None:
+        if node is None:
             node = self.root
 
-        elif node.left_child == None and node.right_child == None:
-            print(node)
-            return
-        else:
-            if node.left_child != None:
-                return(self.search(lower, upper, node.left_child))
+        if node.rating >= lower and node.rating <= upper:
+            matches.append(node)
 
-            if node.right_child != None:
-                return(self.search(lower, upper, node.right_child))
+        elif node.left_child == None and node.right_child == None:
+            return
+
+        elif node.left_child is not None and node.left_child >= lower:
+            return(self.search(lower, upper, node.left_child))
+
+        elif node.right_child is not None and node.right_child <= upper:
+            return(self.search(lower, upper, node.right_child))
